5ccd6d3abd8ab20f20f8581cbea6258726ff4c98
Mao-Lin Wang <focaaby@gmail.com>
2024-07-12 11:25:22 +0800

    Migrate to docusaurus

---
+++ docusaurus.config.ts
@@ -0,0 +1,113 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: '數位韌性教材專區',
+  tagline: '系統在變動或極端的環境下，持續提供美好的服務，並能在故障時優雅復原',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://nics-tw.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'nics-tw', // Usually your GitHub org/user name.
+  projectName: 'resilience-material', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-Hant',
+    locales: ['zh-Hant'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          routeBasePath: 'material',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/nics-tw/resilience-material/tree/main/docs',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: '數位韌性教材專區',
+      logo: {
+        alt: '數位韌性教材 Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'materialSidebar',
+          position: 'left',
+          label: '教材',
+        },
+        {
+          href: 'https://github.com/nics-tw/resilience-material',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: '教材',
+          items: [
+            {
+              label: '數位韌性領航員培訓課程教材',
+              to: '/material/training',
+            },
+            {
+              label: '共通性建議教材',
+              to: '/material/category/共同性教材',
+            }
+          ],
+        },
+        {
+          title: '相關連結',
+          items: [
+            {
+              label: '政府網站設計原則',
+              href: 'https://guide.nics.nat.gov.tw/'
+            },
+            {
+              label: 'SBOM 銜接 VANS 規格工具',
+              href: 'https://github.com/nics-tw/sbom2vans',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()}  <a href="https://creativecommons.org/licenses/by-nd/4.0/deed.en" target="_blank">CC-BY-ND-4.0</a>. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,67 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  description: JSX.Element;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: '易用性',
+    Svg: require('@site/static/img/undraw_experience_design.svg').default,
+    description: (
+      <>
+        讓系統更貼近使用者，提供友善的使用者介面，讓使用者能夠上手提高無障礙界面。
+      </>
+    ),
+  },
+  {
+    title: '可維護性',
+    Svg: require('@site/static/img/undraw_engineering_team.svg').default,
+    description: (
+      <>
+        提高程式碼的掌控權，建立自動化 CI/CD 流程，包含自動化產生 SBOM、程式碼檢查、程式碼覆蓋率等。
+      </>
+    ),
+  },
+  {
+    title: '高可用性',
+    Svg: require('@site/static/img/undraw_server_down.svg').default,
+    description: (
+      <>
+        建制高可用性的系統，其 RTO/RPO 數值應符合自動化備份、DR site 恢復等，提供系統穩定性。
+      </>
+    ),
+  },
+];
+
+function Feature({title, Svg, description}: FeatureItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
