e0731825cfa0a20306603add342f3a55abcb945d
Mao-Lin Wang <focaaby@gmail.com>
2024-07-18 16:39:58 +0800

    Update DNS name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://nics-tw.github.io',
+  url: 'https://material.nics.nat.gov.tw',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/resilience-material/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
