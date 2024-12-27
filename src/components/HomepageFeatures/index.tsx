3f97da4bc0d531949cabcc9379c89b68e23fdc9d
William Cheng <asd869zxc@hotmail.com.tw>
2024-12-27 15:36:36 +0800

    fix: resolve deployment failure and switch sidebar to manual configuration

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -4,59 +4,57 @@ import styles from './styles.module.css';
 import Link from '@docusaurus/Link';
 
 type FeatureItem = {
-  title: string;
+  title: string; // 將 title 保留為純文字
   Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: JSX.Element;
+  link: string; // 增加一個 link 屬性來指定超連結
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: (
-      <Link to="/material/common/usability">易用性</Link>
-    ),
+    title: '易用性',
     Svg: require('@site/static/img/undraw_experience_design.svg').default,
     description: (
       <>
         讓系統更貼近使用者，提供友善的使用者介面，讓使用者能夠上手提高無障礙界面。
       </>
     ),
+    link: '/material/accessbility',
   },
   {
-    title: (
-      <Link to="/material/common/maintainable">可維護性</Link>
-    ),
+    title: '可維護性',
     Svg: require('@site/static/img/undraw_engineering_team.svg').default,
     description: (
       <>
         提高程式碼的掌控權，建立自動化 CI/CD 流程，包含自動化產生 SBOM、程式碼檢查、程式碼覆蓋率等。
       </>
     ),
+    link: '/material/maintainable',
   },
   {
-    title: (
-      <Link to="/material/common/high-availability">高可用性</Link>
-    ),
+    title: '高可用性',
     Svg: require('@site/static/img/undraw_server_down.svg').default,
     description: (
       <>
         建制高可用性的系統，其 RTO/RPO 數值應符合自動化備份、DR site 恢復等，提供系統穩定性。
       </>
     ),
+    link: '/material/high-availability',
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
-  const linkPath = (title as JSX.Element).props.to;
-  
+function Feature({title, Svg, description, link}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Link to={linkPath}>
-        <Svg className={styles.featureSvg} role="img" />
+        <Link to={link}>
+          <Svg className={styles.featureSvg} role="img" />
         </Link>
       </div>
       <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
+        <Heading as="h3">
+          <Link to={link}>{title}</Link>
+        </Heading>
         <p>{description}</p>
       </div>
     </div>
