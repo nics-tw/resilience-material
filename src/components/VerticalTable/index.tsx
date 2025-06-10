b6d9686f0415c43a7c6944ec2bec228ff9f820d4
tony140407 <a32715368@gmail.com>
2025-06-10 15:31:51 +0800

    feat: add VerticalTable and YoutubeEmbed components

---
+++ src/components/VerticalTable/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+export default function VerticalTable({
+  data,
+}: {
+  data: Record<string, React.ReactNode>;
+}) {
+  return (
+    <section>
+      <table>
+        <tbody>
+          {Object.entries(data).map(([key, value]) => (
+            <tr key={key}>
+              <th className={styles.verticalTableHeader}>{key}</th>
+              <td>{value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
+}
---
+++ src/components/YoutubeEmbed/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export interface YoutubeEmbedProps {
+  videoId: string;
+  width?: number | string;
+  height?: number | string;
+}
+
+export default function YoutubeEmbed({
+  videoId,
+  width = 560,
+  height = 315,
+}: YoutubeEmbedProps) {
+  return (
+    <iframe
+      width={width}
+      height={height}
+      src={`https://www.youtube-nocookie.com/embed/${videoId}`}
+      title="YouTube video player"
+      frameBorder="0"
+      loading="lazy"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowFullScreen
+      sandbox="allow-scripts allow-same-origin allow-presentation allow-popups allow-forms"
+      style={{ maxWidth: "100%" }}
+    />
+  );
+}
